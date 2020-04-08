597d9f455d3ad5a15757726a285b9110deab6457
syahruddin <syahruddinr@gmail.com>
2020-04-09 03:41:52 +0700

    initial

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, jsonify, render_template, redirect, url_for, flash
+import requests
+app = Flask(__name__)
+
+@app.route('/', methods=['POST','GET'])
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
