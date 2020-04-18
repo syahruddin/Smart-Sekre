28fbf2ce7528472036b8ef4c0b69a5d2d2806e9b
syahruddin <syahruddinr@gmail.com>
2020-04-18 23:44:19 +0700

    coba login simpel

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask, request, jsonify, render_template, redirect, url_for, flash
+from flask import Flask, request, jsonify,session, render_template, redirect, url_for, flash
 import requests
 app = Flask(__name__)
 
 @app.route('/', methods=['POST','GET'])
 def index():
-    return render_template('index.html')
+    if not session.get('logged_in'):
+        return render_template('index.html')
+    else:
+        return viewSekre()
 
+@app.route('/login',methods=['POST'])
+def login():
+    if request.form['password'] == 'kemahasiswaan' and request.form['username'] == 'lkitb':
+        session['logged_in'] = True
+    else:
+        flash('Username/Password Salah!')
+    return index()
 
 @app.route('/view', methods=['POST','GET'])
 def viewSekre():
