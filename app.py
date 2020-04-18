ba0184d4ee7eff515111ccdafc907b535ff3c872
syahruddin <syahruddinr@gmail.com>
2020-04-19 02:15:29 +0700

    coba

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 from flask import Flask, request, jsonify,session, render_template, redirect, url_for, flash,abort
 import requests
-from flask.ext.session import Session
 import os
 app = Flask(__name__)
-sess = Session()
 
 @app.route('/', methods=['POST','GET'])
 def index():
     if request.method == 'POST':
         if request.form['password'] == 'kemahasiswaan' and request.form['username'] == 'lkitb':
             session['logged_in'] = True
+            return index()
         else:
             flash('Username/Password Salah!')
-        return index()
+            return index()
     else:
         if not session.get('logged_in'):
             return render_template('index.html')
@@ -26,8 +25,5 @@ def viewSekre():
 
 
 if __name__ == '__main__':
-    app.secret_key = '500505'
-    app.config['SESSION_TYPE'] = 'filesystem'
-    # Threaded option to enable multiple instances for multiple user access support
-    sess.init_app(app)
+    app.secret_key = os.urandom(12)
     app.run(threaded=True, port=5000)
